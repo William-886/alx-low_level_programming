c790fdcf90d32544384251f6365879b710947c88
William-886 <Kingliamm93@gmail.com>
2022-08-07 15:57:53 +0000

    Assignment complete

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * Main - print a quote without using printf or puts
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+Return 1;
+}
