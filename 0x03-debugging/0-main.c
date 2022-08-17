38a96a90ee1879c7760187ed36bda7f3b1efe2c1
William-886 <Kingliamm93@gmail.com>
2022-08-17 00:15:02 +0000

    assignment done

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: Test positive_or_negative function
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i = 0;
+positive_or_negative(i);
+return (0);
+}
