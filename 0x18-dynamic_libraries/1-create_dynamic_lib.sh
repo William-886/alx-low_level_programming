6b2eaf6438f78b2da804821d583a099a58885e4c
William-886 <Kingliamm93@gmail.com>
2022-09-05 23:32:53 +0000

    Hot fix

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/kjnat/alx-low_level_programming/main/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+gcc -Wall -Werror -Wextra -pedantic -fPIC *.c -shared -o liball.so
