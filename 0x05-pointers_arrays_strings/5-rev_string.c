5b31caa498814ba27f42faead6a9a6ef12468408
William-886 <kingliamm93@gmail.com>
2023-08-15 16:36:40 +0000

    lezzgo deleted

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,8 +2,21 @@
 #include "main.h"
 
 /**
- * print_rev - function to print a string in reverse
+ * print_rev - function that reverses a string
  * @s: input of type char
  * Return: String in reverse
  */
 
+void rev_string(char *s)
+{
+int i, count = 0;
+char a;
+for(i = 0; *(s + i) != '\0'; i++)
+    count++;
+
+for(i = count - 1; i >= 0; i--)
+{
+    a = *(s + i);
+    return (a);
+}
+}
